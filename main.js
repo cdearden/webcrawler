2445fef7d3f93df6be4a7b8fe5f8de52841da988
Hack Reactor Students <accounts@hackreactor.com>
2016-11-04 20:13:15 -0700

    Retrieve an html document string given a website.

--- main.js
+++ main.js
@@ -2,28 +2,17 @@
 // Questions: 
 // Should I use jQuery? If I use jQuery and this code folows a 
 // link to a website without jQuery will that be a problem?
-var crawl = function (url, config) {
-  var num = 0;
-  
-  // var src = URL.createObjectURL(url);
-  // var doc = window.document; 
-  // var children = root.childNodes;
-
-  location.assign(url);
-  console.log(url);
-
-  document.ready();
-  var links = document.links;
 
 
-  for ( var i = num ; i < links.length ; i++ ) {
-     console.log( links[i].href );
-  }
+var request = require('request');
 
-  // for ( var i = num ; i < links.length ; i++ ) {
-  //    crawl( links[i].href );
-  // }
-  
+var seed = 'https://www.kirupa.com/html5/making_http_requests_js.htm';
 
-};
+request(seed, function(error, response, body) {
+    if ( !error ) {
+      console.log(body);
+    }  
+});
+
+console.log('out of response');
 
