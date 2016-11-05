6aad382bd98f433b7a7f81f372a367226202eea6
Hack Reactor Students <accounts@hackreactor.com>
2016-11-04 21:37:45 -0700

    Use cheerio module to parse raw html and returen links.

--- main.js
+++ main.js
@@ -4,15 +4,27 @@
 // link to a website without jQuery will that be a problem?
 
 
+
 var request = require('request');
+var cheerio = require('cheerio');
 
 var seed = 'https://www.kirupa.com/html5/making_http_requests_js.htm';
 
 request(seed, function(error, response, body) {
     if ( !error ) {
       console.log(body);
+      getLinks(body);
     }  
 });
 
 console.log('out of response');
 
+var getLinks = function (htmlString) {
+  $ = cheerio.load(htmlString);
+  
+  var links = $('a').map(function(element, index) {
+    return $(this).attr('href');
+  }).get();
+
+  return links;
+};
